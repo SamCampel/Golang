4e8e66b329b4326e8b1a7b4ca2d01d6ca4dad2bc
Samuel Campelo <168687328+SamCampel@users.noreply.github.com>
2025-04-03 21:43:50 -0300

    Create 08Exercicios_01.go

---
+++ 08Exercicios_01.go
@@ -0,0 +1,19 @@
+// Utilizando o operador curto de declaração, atribuia esses valores com identificadores "x", "y" e "z" - 42, José e true.
+// Depois demonstre os valor em um único print e multiplos prints
+
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	y := "José"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+}
